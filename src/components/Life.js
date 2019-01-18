0261ebd5ec136b55294793ccd9f7df574b6f67b8
Gary Irick <garyirick@gmail.com>
2019-01-18 13:05:41 -0600

    Added comment.

--- src/components/Life.js
+++ src/components/Life.js
@@ -10,6 +10,9 @@ const Life = ({ match, population, initSize }) => {
   // E_NOTIMPL: Is this the right place to check to see if we need
   // to be initialized?  I need the URL parameter, but it feels weird.
   if (population.length === 0) {
+    // E_NOTIMPL: Does it make more sense to use withRouter() instead of
+    // match.params here?
+
     // We assume here that the URL has a valid number or nothing, we should
     // really check that.
     const size = match.params.gridSize ? match.params.gridSize : defaultSize
