7eefd0146b1fcd4ec5fe8fdc3b10971ac38e413b
Gary Irick <garyirick@gmail.com>
2019-01-18 11:49:45 -0600

    Installed redux-thunk.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { Route } from 'react-router'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import 'font-awesome/css/font-awesome.css'
 import './styles/styles.scss'
 import rootReducer from './reducers'
 import Life from './components/Life'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 render((
   <Provider store={store}>
