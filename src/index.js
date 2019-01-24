50e65488d7759f0adb86a0724074d3104e929379
Gary Irick <garyirick@gmail.com>
2019-01-24 10:49:54 -0600

    Added redux-logger.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import promiseMiddleware from 'redux-promise'
+import { createLogger } from 'redux-logger'
 import 'font-awesome/css/font-awesome.css'
 import './styles/styles.scss'
 import rootReducer from './reducers'
 import Life from './components/Life'
 
+const logger = createLogger()
+
 // We use redux-promise for some actions, and redux-thunk for others.  We could
 // probably change this to use only one of them, but I wanted examples of both.
-const store = createStore(rootReducer, applyMiddleware(thunk, promiseMiddleware))
+const store = createStore(rootReducer, applyMiddleware(thunk, promiseMiddleware, logger))
 
 render((
   <Provider store={store}>
